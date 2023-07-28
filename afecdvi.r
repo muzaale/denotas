deef63bc01ed2f7d713a25ed40492358daa4627a
muzaale <muzaale@gmail.com>
2023-07-28 04:33:36 -0400

    r extensions for vscode documentation

--- afecdvi.r
+++ afecdvi.r
@@ -1,1 +1,16 @@
-1 + 1
+c(1:100) # how to run an R script from the command line?
+
+# Path: bloc/afecdvi.r
+
+# how to execute bloc/afecdvi.r from the command line? step-by-step:
+# 1. open a terminal
+# 2. cd to the directory where the script is located
+# 3. type Rscript afecdvi.r
+
+
+# how to run an R script in a codeblock of jupyter notebook?
+# 1. open a jupyter notebook
+# 2. create a new codeblock
+# 3. type %%R at the top of the codeblock
+# 4. type the R code in the codeblock
+# 5. run the codeblock
