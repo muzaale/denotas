b49f886cde8e5cca823788a071ced4f77064bc83
muzaale <muzaale@gmail.com>
2023-07-30 08:37:39 -0400

    getting used to 5.0 workflow: commits using .sh script, bellissimo!!!!

---
+++ z_shell.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Change directory to ~/dropbox/1f.ἡἔρις,κ/1.ontology
+cd ~/dropbox/1f.ἡἔρις,κ/1.ontology
+
+# Build Jupyter Book with 'gano' as the argument
+jb build bloc
+cp -r bloc/* denotas
+
+# Change directory to 'amagunju'
+cd denotas
+
+# Add all files in the current directory to Git
+git add ./*
+
+# Commit changes to Git with the given commit message
+git commit -m "getting used to 5.0 workflow: commits using .sh script, bellissimo!!!!"
+
+# Push changes to GitHub
+# git remote set-url origin git@github.com:muzaale/amagunju
+# ngalabi
+
+git push 
+ghp-import -n -p -f _build/html
+
+
+
+
+
+
