6a2df9be787ab67f836c0b7dc34e3d3377b30732
muzaale <muzaale@gmail.com>
2023-07-30 16:25:00 -0400

    she's my only girl -- only loop. thanks liz2ra!!!!

---
+++ blocdenotas.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Change directory to ~/dropbox/1f.ἡἔρις,κ/1.ontology
+cd ~/dropbox/1f.ἡἔρις,κ/1.ontology
+
+# Build Jupyter Book with 'gano' as the argument
+jb build bloc
+cp -r bloc/* denotas
+
+# Change directory to 'amagunju'
+cd denotas
+
+# Add all files in the current directory to Git
+git add ./*
+
+# Commit changes to Git with the given commit message
+git commit -m "she's my only girl -- only loop. thanks liz2ra!!!!"
+
+# Push changes to GitHub
+# git remote set-url origin git@github.com:muzaale/amagunju
+# ngalabi
+
+git push 
+ghp-import -n -p -f _build/html
+
+
+
+
+
+
