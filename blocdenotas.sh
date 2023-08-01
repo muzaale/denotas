f3dc76fdc79d164fd27bd4112b27d7cca81db09a
muzaale <muzaale@gmail.com>
2023-07-31 20:59:04 -0400

    Shakespeare Contra Nietzsche by Andrew Lanham: fave essay

--- blocdenotas.sh
+++ blocdenotas.sh
@@ -14,7 +14,7 @@ cd denotas
 git add ./*
 
 # Commit changes to Git with the given commit message
-git commit -m "array, list, tibble"
+git commit -m "Shakespeare Contra Nietzsche by Andrew Lanham: fave essay"
 
 # Push changes to GitHub
 # git remote set-url origin git@github.com:muzaale/amagunju
