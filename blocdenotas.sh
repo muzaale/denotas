bf6bebc91f602fc0505de18744d4a879c0689431
muzaale <muzaale@gmail.com>
2023-08-02 12:55:23 -0400

    notfancybutworks three40.sh, take2

--- blocdenotas.sh
+++ blocdenotas.sh
@@ -14,7 +14,7 @@ cd denotas
 git add ./*
 
 # Commit changes to Git with the given commit message
-git commit -m "notfancybutworks three40.sh"
+git commit -m "notfancybutworks three40.sh, take2"
 
 # Push changes to GitHub
 # git remote set-url origin git@github.com:muzaale/amagunju
