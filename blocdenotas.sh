167a39accdc5adc599c8977e380d7e7d4acf10c4
muzaale <muzaale@gmail.com>
2023-08-03 18:51:11 -0400

    #860: pwomd

--- blocdenotas.sh
+++ blocdenotas.sh
@@ -7,7 +7,7 @@ cd ~/dropbox/1f.ἡἔρις,κ/1.ontology
 jb build bloc
 cp -r bloc/* denotas
 
-# Change directory to 'amagunju'
+# Change directory to 'denotas'
 cd denotas
 
 # Add all files in the current directory to Git
@@ -16,12 +16,15 @@ git add ./*
 # Commit changes to Git with the given commit message
 git commit -m "#860: pwomd"
 
-# Push changes to GitHub
-# git remote set-url origin git@github.com:muzaale/amagunju
-# ngalabi
+# Ensure using the SSH URL for the repository
+git remote set-url origin git@github.com:muzaale/denotas.git
 
-git push 
+# Push changes to GitHub
+git push origin main
+
+# Import the built HTML to gh-pages and push to GitHub
 ghp-import -n -p -f _build/html
+
 
 
 
