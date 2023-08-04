7e6711bb10c26682e7492307a0649a467750d728
muzaale <muzaale@gmail.com>
2023-08-04 10:05:34 -0400

    adversarial-learning: i am the generator, you are the discriminator

--- blocdenotas.sh
+++ blocdenotas.sh
@@ -4,7 +4,7 @@ jb build bloc
 cp -r bloc/* denotas
 cd denotas
 git add ./*
-git commit -m "database scheme "
+git commit -m "adversarial-learning: i am the generator, you are the discriminator"
 chmod 600 ~/.ssh/id_blocdenotas
 git remote -v
 ssh-add -D
