6988d8606d916412a525c344605268b462db9e00
muzaale <muzaale@gmail.com>
2023-08-04 07:52:24 -0400

    new, combined ./gh.sh

--- blocdenotas.sh
+++ blocdenotas.sh
@@ -4,7 +4,7 @@ jb build bloc
 cp -r bloc/* denotas
 cd denotas
 git add ./*
-git commit -m "git log"
+git commit -m "new, combined ./gh.sh"
 chmod 600 ~/.ssh/id_blocdenotas
 git remote -v
 ssh-add -D
