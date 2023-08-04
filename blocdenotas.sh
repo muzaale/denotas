28938958e9acd972e1b13d733fff6b1f5c852616
muzaale <muzaale@gmail.com>
2023-08-04 10:42:30 -0400

    ### 877. fatal-error

--- blocdenotas.sh
+++ blocdenotas.sh
@@ -4,7 +4,7 @@ jb build bloc
 cp -r bloc/* denotas
 cd denotas
 git add ./*
-git commit -m "adversarial-learning: i am the generator, you are the discriminator"
+git commit -m "### 877. fatal-error"
 chmod 600 ~/.ssh/id_blocdenotas
 git remote -v
 ssh-add -D
