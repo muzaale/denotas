e078e2e0c9a93e90b6a28704109fce45e8897428
muzaale <muzaale@gmail.com>
2023-08-04 01:07:47 -0400

    moving .git history

--- blocdenotas.sh
+++ blocdenotas.sh
@@ -4,7 +4,7 @@ jb build bloc
 cp -r bloc/* denotas
 cd denotas
 git add ./*
-git commit -m "housekeeping"
+git commit -m "moving .git history"
 chmod 600 ~/.ssh/id_blocdenotas
 git remote -v
 ssh-add -D
