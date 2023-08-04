7ffadd4d22a5548ce6cd98f1d0b118b4fd7ed3d7
muzaale <muzaale@gmail.com>
2023-08-04 09:56:29 -0400

    database scheme

--- blocdenotas.sh
+++ blocdenotas.sh
@@ -4,7 +4,7 @@ jb build bloc
 cp -r bloc/* denotas
 cd denotas
 git add ./*
-git commit -m "workflow 7.4, iteration... 4: blank canvas "
+git commit -m "database scheme "
 chmod 600 ~/.ssh/id_blocdenotas
 git remote -v
 ssh-add -D
