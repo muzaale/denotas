81ae9c731841a56b782e9393fefd953fad6a069b
muzaale <muzaale@gmail.com>
2023-08-04 08:39:42 -0400

    workflow 7.3, iteration... 3

--- blocdenotas.sh
+++ blocdenotas.sh
@@ -4,7 +4,7 @@ jb build bloc
 cp -r bloc/* denotas
 cd denotas
 git add ./*
-git commit -m "workflow 7.2, iteration... 2"
+git commit -m "workflow 7.3, iteration... 3"
 chmod 600 ~/.ssh/id_blocdenotas
 git remote -v
 ssh-add -D
