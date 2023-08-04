cda91bfb217b104ba0ab310375ee1f93d7303f88
muzaale <muzaale@gmail.com>
2023-08-03 21:12:31 -0400

    restored uncondtional syntax for stata class, but left some untidy stuff. remove it!

--- blocdenotas.sh
+++ blocdenotas.sh
@@ -4,7 +4,7 @@ jb build bloc
 cp -r bloc/* denotas
 cd denotas
 git add ./*
-git commit -m "restore uncondtional syntax for stata class"
+git commit -m "restored uncondtional syntax for stata class, but left some untidy stuff. remove it!"
 chmod 600 ~/.ssh/id_blocdenotas
 git remote -v
 ssh-add -D
