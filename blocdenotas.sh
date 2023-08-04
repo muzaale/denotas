c19f8f295bc5552d05d1e4d855c64c4b853029eb
muzaale <muzaale@gmail.com>
2023-08-03 20:30:27 -0400

    restore uncondtional syntax for stata class

--- blocdenotas.sh
+++ blocdenotas.sh
@@ -4,7 +4,7 @@ jb build bloc
 cp -r bloc/* denotas
 cd denotas
 git add ./*
-git commit -m "double-check that this routine works, now with SSH key"
+git commit -m "restore uncondtional syntax for stata class"
 chmod 600 ~/.ssh/id_blocdenotas
 git remote -v
 ssh-add -D
