c554efa665846eb8908ff2ad03d05bd5904f4ed8
muzaale <muzaale@gmail.com>
2023-08-05 09:45:28 -0400

    game changer workflow: ### 882

--- blocdenotas.sh
+++ blocdenotas.sh
@@ -4,7 +4,7 @@ jb build bloc
 cp -r bloc/* denotas
 cd denotas
 git add ./*
-git commit -m "the sweet in sweet chilli-fried whole jerk snapper!!"
+git commit -m "game changer workflow: ### 882"
 chmod 600 ~/.ssh/id_blocdenotas
 git remote -v
 ssh-add -D
