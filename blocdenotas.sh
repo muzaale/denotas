cbeb7deba33d4ab6f8801296bef0b989b10b1c5a
muzaale <muzaale@gmail.com>
2023-08-05 19:30:13 -0400

    victory!

--- blocdenotas.sh
+++ blocdenotas.sh
@@ -4,7 +4,7 @@ jb build bloc
 cp -r bloc/* denotas
 cd denotas
 git add ./*
-git commit -m "The error you're experiencing seems to originate from the _toc.yml file format. The error message indicates:"
+git commit -m "victory!"
 chmod 600 ~/.ssh/id_blocdenotas
 git remote -v
 ssh-add -D
