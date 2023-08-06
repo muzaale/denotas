82a797b5b890a67e1a69bdbe7d5b84573b1f083c
muzaale <muzaale@gmail.com>
2023-08-06 08:23:56 -0400

    fancy: # Path to the Python interpreter that works python_path=/usr/local/anaconda3/bin/python

--- blocdenotas.sh
+++ blocdenotas.sh
@@ -1,22 +1,5 @@
 #!/bin/bash
 
-# Path to the Python interpreter that works
-python_path="/usr/local/anaconda3/bin/python"
-
-# Check if the specified path is indeed a Python 3 interpreter
-if $python_path -c 'import sys; exit(sys.version_info < (3,))' 2>/dev/null; then
-    echo "Found Python 3 interpreter at $python_path"
-
-    # Install anytree using pip corresponding to the specified Python interpreter
-    echo "Installing anytree for $python_path"
-    $python_path -m pip install anytree
-else
-    echo "The specified path does not point to a Python 3 interpreter. Aborting."
-fi
-
-echo "Done."
-
-
 pwd
 cd ~/dropbox/1f.ἡἔρις,κ/1.ontology 
 jb build bloc
