cb3d0a25829dcd88dd8861b2591c813012eecb84
muzaale <muzaale@gmail.com>
2023-08-06 11:00:07 -0400

    try tree lib after installing directed packages

--- blocdenotas.sh
+++ blocdenotas.sh
@@ -6,7 +6,7 @@ jb build bloc
 cp -r bloc/* denotas
 cd denotas
 git add ./*
-git commit -m "install packages in specified python environments"
+git commit -m "try tree lib after installing directed packages"
 chmod 600 ~/.ssh/id_blocdenotas
 git remote -v
 ssh-add -D
