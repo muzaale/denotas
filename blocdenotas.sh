5032a55609a16a9b18400f6bdf7136ce091f3ae5
muzaale <muzaale@gmail.com>
2023-08-05 23:48:33 -0400

    ### 892. ip.sh-finale; Variant of the script that includes source path for  file:

--- blocdenotas.sh
+++ blocdenotas.sh
@@ -4,7 +4,7 @@ jb build bloc
 cp -r bloc/* denotas
 cd denotas
 git add ./*
-git commit -m "### 892. ip.sh-finale; lotta user input required upfront!!"
+git commit -m "### 892. ip.sh-finale; Variant of the script that includes source path for `populate_be.ipynb` file:"
 chmod 600 ~/.ssh/id_blocdenotas
 git remote -v
 ssh-add -D
