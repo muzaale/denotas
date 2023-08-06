2bc17ba9dff94ef7c0c06a3ba754ca690d783c55
muzaale <muzaale@gmail.com>
2023-08-06 00:30:40 -0400

    ### 894. agift2me
    spjd note-taking & updates import nbformat
    import os
    
    # Define the path to the input and output files
    input_path = os.path.expanduser('~/dropbox/1f.ἡἔρις,κ/1.ontology/be/be_6/be_i7_8.ipynb')
    output_path = os.path.expanduser('~/dropbox/1f.ἡἔρις,κ/1.ontology/populate_fe.ipynb')

--- blocdenotas.sh
+++ blocdenotas.sh
@@ -4,7 +4,14 @@ jb build bloc
 cp -r bloc/* denotas
 cd denotas
 git add ./*
-git commit -m "## 08/06/2023, ### 893. agift2me"
+git commit -m "### 894. agift2me
+
+spjd note-taking & updates import nbformat
+import os
+
+# Define the path to the input and output files
+input_path = os.path.expanduser('~/dropbox/1f.ἡἔρις,κ/1.ontology/be/be_6/be_i7_8.ipynb')
+output_path = os.path.expanduser('~/dropbox/1f.ἡἔρις,κ/1.ontology/populate_fe.ipynb')"
 chmod 600 ~/.ssh/id_blocdenotas
 git remote -v
 ssh-add -D
