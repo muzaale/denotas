98f1720c57157e1c89f4d0b0416f5c790f1a69ad
muzaale <muzaale@gmail.com>
2023-08-07 02:08:56 -0400

    ### 901. colab

--- blocdenotas.sh
+++ blocdenotas.sh
@@ -6,7 +6,7 @@ jb build bloc
 cp -r bloc/* denotas
 cd denotas
 git add ./*
-git commit -m "### 900. No epilogue, I pray you; for your play needs no excuse"
+git commit -m "### 901. colab"
 chmod 600 ~/.ssh/id_blocdenotas
 git remote -v
 ssh-add -D
