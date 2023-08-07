556a45eb037e8265160658d399cd941cf445d63a
muzaale <muzaale@gmail.com>
2023-08-07 07:36:34 -0400

    fixed the music21 issue, by installing it in the bloc/denotas environment - within a jupyter notebook - and then copying the music21 folder to the bloc/denotas environment

--- blocdenotas.sh
+++ blocdenotas.sh
@@ -6,8 +6,7 @@ jb build bloc
 cp -r bloc/* denotas
 cd denotas
 git add ./*
-git commit -m "### 840. learning, Installing collected packages: webcolors, urllib3, more-itertools, jsonpickle, joblib, idna, charset-normalizer, chardet, certifi, requests, music21
-Successfully installed certifi-2023.7.22 chardet-5.2.0 charset-normalizer-3.2.0 idna-3.4 joblib-1.3.1 jsonpickle-3.0.1 more-itertools-10.1.0 music21-9.1.0 requests-2.31.0 urllib3-2.0.4 webcolors-1.13"
+git commit -m "fixed the music21 issue, by installing it in the bloc/denotas environment - within a jupyter notebook - and then copying the music21 folder to the bloc/denotas environment"
 chmod 600 ~/.ssh/id_blocdenotas
 git remote -v
 ssh-add -D
