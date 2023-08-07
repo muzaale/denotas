c7cdf8f642cce23f41a9fafd6ecaed54c170fe99
muzaale <muzaale@gmail.com>
2023-08-07 15:24:24 -0400

    ### 907. gmu: urls added for -- didactic pdfs on intervals, scales, chords, modes, and keys.

--- blocdenotas.sh
+++ blocdenotas.sh
@@ -6,7 +6,7 @@ jb build bloc
 cp -r bloc/* denotas
 cd denotas
 git add ./*
-git commit -m "### 907. gmu: didactic pdfs on intervals, scales, chords, modes, and keys."
+git commit -m "### 907. gmu: urls added for -- didactic pdfs on intervals, scales, chords, modes, and keys."
 chmod 600 ~/.ssh/id_blocdenotas
 git remote -v
 ssh-add -D
