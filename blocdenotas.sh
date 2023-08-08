42765b1755ca47427548de896a0bd2887f1d4db4
muzaale <muzaale@gmail.com>
2023-08-08 03:15:41 -0400

    ### 913. philosophe pitch!

--- blocdenotas.sh
+++ blocdenotas.sh
@@ -6,7 +6,7 @@ jb build bloc
 cp -r bloc/* denotas
 cd denotas
 git add ./*
-git commit -m "success after maybe 100 failures, no exaggeration"
+git commit -m "### 913. philosophe pitch!"
 chmod 600 ~/.ssh/id_blocdenotas
 git remote -v
 ssh-add -D
