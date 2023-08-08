e8321173d6abd5d6c3d9bce9d9eddf652a5f3030
muzaale <muzaale@gmail.com>
2023-08-07 22:15:14 -0400

    ### 909. gchist.sh - get it to work!

--- blocdenotas.sh
+++ blocdenotas.sh
@@ -6,7 +6,7 @@ jb build bloc
 cp -r bloc/* denotas
 cd denotas
 git add ./*
-git commit -m "### 908. gd.sh is the #1 program in the world."
+git commit -m "### 909. gchist.sh - get it to work!"
 chmod 600 ~/.ssh/id_blocdenotas
 git remote -v
 ssh-add -D
