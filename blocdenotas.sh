8d1476b41f2f9a47cb04645db41bb532dd01dcf8
muzaale <muzaale@gmail.com>
2023-08-08 03:27:42 -0400

    ### 912. bmv - somethin to do with bach

--- blocdenotas.sh
+++ blocdenotas.sh
@@ -6,7 +6,7 @@ jb build bloc
 cp -r bloc/* denotas
 cd denotas
 git add ./*
-git commit -m "### 913. philosophe pitch!"
+git commit -m "### 912. bmv - somethin to do with bach"
 chmod 600 ~/.ssh/id_blocdenotas
 git remote -v
 ssh-add -D
