cb89c2f423bb80bda7914c5c4718e4805222b067
muzaale <muzaale@gmail.com>
2023-08-07 23:46:43 -0400

    success after maybe 100 failures, no exaggeration

--- blocdenotas.sh
+++ blocdenotas.sh
@@ -6,7 +6,7 @@ jb build bloc
 cp -r bloc/* denotas
 cd denotas
 git add ./*
-git commit -m "### 910 tastes like victory, finally!!!"
+git commit -m "success after maybe 100 failures, no exaggeration"
 chmod 600 ~/.ssh/id_blocdenotas
 git remote -v
 ssh-add -D
